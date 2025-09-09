2fed40716ba42a4e1fdeedc4b86c1df14ba69167
bauticalvo <bito.bc@gmail.com>
2025-09-09 10:22:12 -0300

    firts commit

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,7 +3,14 @@ import './styles.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  title: 'Tifosi Cuore',
+  icon: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      url: '/tifosi_logo.png',
+    },
+  ],
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
